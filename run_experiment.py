0141901ceddedb65264e7fb747366fb4fb6949cb
Rutger Rijnenberg <rjrrijnenberg@gmail.com>
2024-03-29 11:36:55 +0100

    Added commandline parsing

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,15 @@
 from policies import Policy
 from plot_learning import plot_cartpole_learning    
+from argparse import ArgumentParser 
 
 
 if __name__ == "__main__":
     num_epochs = 500
     num_repetitions = 3
+    parser = ArgumentParser(description="""DQN agent training.""")
+    parser.add_argument("-no_target_network", dest='target_network', action='store_false', help="""Use a target network for more stable updates.""")
+    parser.add_argument("-no_experience_replay", dest='experience_replay', action='store_false', help="""Use experience replay to break correlations.""")
+    cmdargs = parser.parse_args()
     model_params = {
             'lr': 5e-4,  
             'exp_param': .9,
@@ -12,8 +17,8 @@ if __name__ == "__main__":
             'batch_size': 256,
             'gamma': 0.995,
             'target_network_update_time': 100,
-            'do_target_network': True,
-            'do_experience_replay': True,
+            'do_target_network': cmdargs.target_network,
+            'do_experience_replay': cmdargs.experience_replay,
             'buffer_capacity': 10000,   # == buffer_capacity
             'eval_interval': 10,
             'n_eval_episodes': 10,
@@ -21,7 +26,6 @@ if __name__ == "__main__":
             'anneal_timescale': 100,
             'early_stopping_reward': 500,
     }
-    
     plot_cartpole_learning(num_epochs = num_epochs, 
                            num_repetitions = num_repetitions, 
                            model_params = model_params,
